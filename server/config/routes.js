9c44cd7b44f9505d750ae1eae4387fca36b7b42d
Kinjal Chatterjee <kinjalchatterjee@gmail.com>
2016-08-27 15:19:52 -0700

    separated middleware out of server.js

--- server/config/routes.js
+++ server/config/routes.js
@@ -1,38 +1,38 @@
 const express = require('express');
 const router = express.Router();
 
-router.use('/', express.static('./client'));
-
-router.get('/dummyData', (req, res)=>{
-  res.send(200, {hello:'world'});
+router.get('/dummyData', (req, res) => {
+  res.send(200, {
+    hello: 'world'
+  });
 });
 
-router.get('/recent', (req, res)=>{
+router.get('/recent', (req, res) => {
   //gets most recent trips
 });
 
-router.post('/signup', (req, res)=>{
+router.post('/signup', (req, res) => {
   //create new user
 });
 
-router.post('/login', (req, res)=>{
+router.post('/login', (req, res) => {
   //gives user acess to account
 });
 
-router.get('/search', (req, res)=>{
+router.get('/search', (req, res) => {
   //uses search params to get revelent results
 });
 
-router.post('/createTrip', (req, res)=>{
+router.post('/createTrip', (req, res) => {
   //creates a new trip
 });
 
-router.post('/reserve', (req, res)=>{
+router.post('/reserve', (req, res) => {
   //reserves seat or seats on a trip
 });
 
-router.get('*', (req, res)=>{
-  res.send(404);
+router.get('*', (req, res) => {
+  res.sendStatus(404);
 });
 
 module.exports = router;
