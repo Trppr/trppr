5cfb2da29a1e4c4ff564ac3a263d74b53e06eb44
SeanCox <sean.cox@mac.com>
2016-08-28 08:03:40 -0700

    added a test login for JWT

--- server/config/middleware.js
+++ server/config/middleware.js
@@ -1,6 +1,8 @@
 const cors = require('cors');
+const bodyParser = require('body-parser');
 
 module.exports = (app, express) => {
   app.use(cors());
+  app.use(bodyParser.urlencoded());
   app.use('/', express.static('./client'));
 };
