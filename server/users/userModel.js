16156bee0037266499158331e2b2a00a790b809e
Kinjal Chatterjee <kinjalchatterjee@gmail.com>
2016-08-28 14:18:50 -0700

    added validation for users model

--- server/users/userModel.js
+++ server/users/userModel.js
@@ -2,29 +2,42 @@ const Sequelize = require('sequelize');
 const sequelize = require('../config/database');
 
 const User = sequelize.define('user', {
+  email: {
+    type: Sequelize.STRING,
+    unique: true,
+    allowNull: false
+  },
+  password: {
+    type:Sequelize.STRING,
+    allowNull: false
+  },
   name: Sequelize.STRING,
-  password: Sequelize.STRING,
-  email: Sequelize.STRING,
-  description: Sequelize.STRING,
-  joinDate: Sequelize.DATE
+  description: Sequelize.TEXT,
 });
 
 sequelize
   .sync() // { force: true } drops table before recreating it
   .then(function(err) {
-    console.log('It worked!');
+    console.log('sync() successful.');
   }, function(err) {
     console.log('An error occurred while creating the table:', err);
   });
 
+// .build() creates a model instance, needs to be saved using .save()
+// .create() builds and saves`
+
 const user = User.build({
   name: "John",
   password: "1234",
-  email: "user@domain.com",
+  email: "user2@domain.com",
   description: "test user data",
   joinDate: Date.now()
 });
 
-user.save().then(function() {
-  console.log("saved!");
+user.save()
+.then(function() {
+  console.log("save() successful");
 })
+.catch(function (err) {
+  console.log('Error:', err);
+});
