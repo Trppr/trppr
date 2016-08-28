801c44fc6d5767a70a341f144db37361c3a33472
Kinjal Chatterjee <kinjalchatterjee@gmail.com>
2016-08-27 17:03:01 -0700

    moved db stuff, added schema

--- server/server.js
+++ server/server.js
@@ -1,22 +1,10 @@
 const express = require('express');
 const routes = require('./config/routes');
-const Sequelize = require('sequelize');
 
 const app = express();
 const port = process.env.PORT || 3000;
 
-sequelize = new Sequelize('postgres://admin:PSZFZHAUDTKOUUHS@sl-us-dal-9-portal.2.dblayer.com:10860/compose');
-sequelize
-.authenticate()
-.then(function(err) {
-  console.log('Connected to DB!');
-})
-.catch(function (err) {
-  console.log('Unable to connect to the database:', err);
-});
-
-
-require('./config/middleware.js')(app, express);
+require('./config/middleware')(app, express);
 app.use('/', routes);
 
 app.listen(port, () => {
