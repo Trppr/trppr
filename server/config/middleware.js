2d7e42f3703cdd0c8c9a83e8073d1ed1c5d022bb
SeanCox <sean.cox@mac.com>
2016-08-28 08:21:03 -0700

    added jwt depencies

--- server/config/middleware.js
+++ server/config/middleware.js
@@ -1,8 +1,14 @@
 const cors = require('cors');
 const bodyParser = require('body-parser');
+const expressJWT = require('express-jwt');
+const jwt = require('jsonwebtoken');
 
 module.exports = (app, express) => {
   app.use(cors());
   app.use(bodyParser.urlencoded());
+  //hide secret later
+  app.use(expressJWT({secret:'hello world trppr'})
+  .unless({path: ['/login', '/', '/recent', '/signup', '/search', '/dummyData']}));
+
   app.use('/', express.static('./client'));
 };
