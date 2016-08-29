4782df8694dc5b4710938a08197ab327825104e6
Kinjal Chatterjee <kinjalchatterjee@gmail.com>
2016-08-28 19:04:48 -0700

    [feature] create user controller method initial implementation

--- server/users/userController.js
+++ server/users/userController.js
@@ -1,5 +1,22 @@
 const user = require('userModel');
 
-// user methods:
-  // create user
-  // get user information
+module.exports = {
+  createUser: function(req, res) {
+    const newUser = User.build({
+      name: req.body.name,
+      password: req.body.password,
+      email: req.body.email,
+      description: req.body.description,
+    });
+    newUser
+      .save()
+      .then(function() {
+        console.log("new user created");
+        res.sendStatus(201);
+      })
+      .catch(function(err) {
+        console.log('Error:', err);
+      });
+  },
+  getUser: function() {}
+}
