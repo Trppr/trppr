51acf418a31163ddc8ffe10ef83e5d486a586be0
Eugene Row <eugene.row@gmail.com>
2016-08-30 15:16:57 -0700

    [feature] trip list and trip component

--- client/src/components/tripList.jsx
+++ client/src/components/tripList.jsx
@@ -1,16 +1,19 @@
 import React from 'react';
 import {render} from 'react-dom';
+import Trip from './trip.jsx';
 
 //Need to make trip const, each trip needs to read each object and
 //display different values based on each attribute on the object
   const TripList = (props) => {
-     const trips = ["Los Angeles - Las Vegas", "New York - Boston", "San Diego - San Francisco", "Portland - Seattle"];
+    console.log("props.trips: ", props.trips);
+     //const trips = ["Los Angeles - Las Vegas", "New York - Boston", "San Diego - San Francisco", "Portland - Seattle"];
      return (
        <div>
          <h3>Trip List</h3>
          <ul>
            {props.trips.map((trip, index) => {
-             return <div key={index}>{trip}</div>
+            // return <div key={index}>{trip}</div>
+            return <Trip trip={trip}/>
            })}
          </ul>
        </div>
