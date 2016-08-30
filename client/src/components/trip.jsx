5aedc6424dcc0c2372a577d9955190ff1469e6d9
Eugene Row <eugene.row@gmail.com>
2016-08-30 15:16:57 -0700

    [feature] trip list and trip component

--- client/src/components/trip.jsx
+++ client/src/components/trip.jsx
@@ -1,5 +1,20 @@
-module.exports = function() {
-  var greet = document.createElement('div');
-  greet.textContent = "Hey I'm inside this div in trip.jsx";
-  return greet;
-};
+import React from 'react';
+import {render} from 'react-dom';
+
+  const Trip = ({trip}) => {
+    console.log("trip insidee trip.jsx: ", trip.driverName);
+     return (
+       <div className="tripEntry">
+            <li>
+            Driver name: {trip.driverName}, &nbsp;
+            Trip dates: {trip.tripDate}, &nbsp;
+            {trip.startLocation} - {trip.endLocation}, &nbsp;
+            Number of seats available: {trip.numSeats}, &nbsp;
+            Price of seat: {trip.seatPrice}, &nbsp;
+            Vehicle Type: {trip.vehicleType}
+            </li>
+       </div>
+     );
+  }
+
+export default Trip;
