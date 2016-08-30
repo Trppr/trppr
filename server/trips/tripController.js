21a2cd0d30e35357b0a336e54fa199024602861d
Kinjal Chatterjee <kinjalchatterjee@gmail.com>
2016-08-30 10:32:58 -0700

    API now has a search route via get, with all optional parameters

--- server/trips/tripController.js
+++ server/trips/tripController.js
@@ -1,4 +1,5 @@
 const Trip = require('../trips/tripModel');
+const sequelize = require('sequelize');
 
 module.exports = {
   createTrip: function(req, res){
@@ -141,6 +142,77 @@ module.exports = {
         tripsList.push(trips[i].dataValues);
       }
       console.log('\033[34m <TRPPR> Sending data: \033[0m');
+      console.log(tripsList);
+      res.json(tripsList);
+    })
+    .catch(function(err) {
+      console.log('Error:', err);
+    });
+  },
+
+  searchTrips: function(req, res){
+    var tripsList = [];
+
+    // Grabs params from query, all of them are optional,
+    // eg a blank query will return all trips
+    var tripDate = req.query.tripDate;
+    var startLocation = req.query.startLocation;
+    var endLocation = req.query.endLocation;
+    var numSeats = req.query.numSeats;
+    var seatPrice = req.query.seatPrice;
+
+    // Creating object that will store attribute queries. This
+    // will create a query only if the attribute is passed in.
+    var where = {};
+
+    if(tripDate){
+      where.tripDate = {
+        // $iLike matches similar string, case-insensitve
+        $iLike: tripDate
+      }
+    }
+    if(startLocation){
+      where.startLocation = {
+        $iLike: startLocation
+      }
+    }
+    if(endLocation){
+      where.endLocation = {
+        $iLike: endLocation
+      }
+    }
+    if(numSeats){
+      where.numSeats = {
+        // $gte matches trips with at least as many seats as the query
+        $gte: numSeats
+      }
+    }
+    if(seatPrice){
+      where.seatPrice = {
+        // $lte matches trips that have prices less than or equal to the query's price
+        $lte: seatPrice
+      }
+    }
+
+    Trip.findAll({
+      where,
+      attributes: [
+        'id',
+        'driverName',
+        'tripDate',
+        'startLocation',
+        'endLocation',
+        'numSeats',
+        'seatPrice',
+        'vehicleType',
+        'description',
+        'driverId'
+      ]
+    })
+    .then(function(trips){
+      for(var i = 0; i < trips.length; i++){
+        tripsList.push(trips[i].dataValues);
+      }
       console.log(tripsList);
       res.json(tripsList);
     })
