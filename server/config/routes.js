409e07e7de2c271883abda187118be014c11e419
Kinjal Chatterjee <kinjalchatterjee@gmail.com>
2016-09-01 10:35:30 -0700

    small update to routes for user api stuff

--- server/config/routes.js
+++ server/config/routes.js
@@ -11,34 +11,11 @@ module.exports = (app, express) => {
     });
   });
 
-  app.get('/recent', (req, res) => {
-    //gets most recent trips
-  });
+  /*
+  *  Use API Requests
+  */
 
-  app.post('/signup', (req, res) => {
-    //create new user
-  });
-
-  // app.post('/login', (req, res) => {
-  //   //checks to make sure we got both a username and password on the request
-  //   //in Postman use body and x-www-form-urlencoded to enter username and password
-  //   if(!req.body.username){
-  //     res.status(400).send('username required');
-  //     return;
-  //   }
-  //   if(!req.body.password){
-  //     res.status(400).send('password required');
-  //     return;
-  //   }
-  //   //test login with JWTs
-  //   if(req.body.username === 'john' && req.body.password === '123'){
-  //     const myToken = jwt
-  //     .sign({ username: req.body.username }, 'hello world trppr');
-  //     res.status(200).json(myToken);
-  //   }else {
-  //     res.status(401).send('invalid login');
-  //   }
-  // });
+  app.post('/signup', userController.createUser);
 
   app.post('/login', userController.authenticateUser);
 
