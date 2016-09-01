22bdfd0032eaf829a8208296af8d402ba4c2f3c7
SeanCox <sean.cox@mac.com>
2016-09-01 12:42:01 -0700

    [feature] turned search into navbar

--- client/src/components/searchBar.jsx
+++ client/src/components/searchBar.jsx
@@ -26,42 +26,53 @@ class SearchBar extends Component {
 
   render() {
     return (
-      <form>
-        <div>
+      <nav className="navbar navbar-default">
+      <div className="container-fluid">
+      <form className="navbar-form navbar-left">
+        <div className="form-group">
           <input
             type = "date"
+            className="form-control"
             placeholder = "Starting date"
             value = {this.state.startDate}
             onChange = {this.handleChange.bind(this, 'startDate')}/>
           <input
             type = "date"
+            className="form-control"
             placeholder = "Ending date"
             value = {this.state.endDate}
             onChange = {this.handleChange.bind(this, 'endDate')}/>
           <input
             placeholder = "Ending city/state"
+            className="form-control"
             value = {this.state.endLocation}
             onChange = {this.handleChange.bind(this, 'endLocation')} />
           <input
             placeholder = "Starting city/state"
+            className="form-control"
             value = {this.state.startLocation}
             onChange = {this.handleChange.bind(this, 'startLocation')} />
           <input
             type = "number"
+            className="form-control"
             placeholder = "# of passengers?"
             value = {this.state.numSeats}
             onChange = {this.handleChange.bind(this, 'numSeats')} />
           <input
             type = "number"
+            className="form-control"
             placeholder = "Max budget?"
             value = {this.state.price}
             onChange = {this.handleChange.bind(this, 'price')}/>
           <input
             type="button"
             value="Search"
-            onClick = {event => this.submitData()}/>
+            onClick = {event => this.submitData()}
+            className="btn btn-primary"/>
         </div>
       </form>
+      </div>
+      </nav>
     )
   }
 }
