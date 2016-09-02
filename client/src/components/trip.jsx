21d1a7c2ab0a8f2c0087f5302956138e0d98448c
Eugene Row <eugene.row@gmail.com>
2016-09-02 14:57:41 -0700

    [fix] trip entries

--- client/src/components/trip.jsx
+++ client/src/components/trip.jsx
@@ -4,18 +4,50 @@ import {render} from 'react-dom';
   const Trip = ({trip}) => {
     console.log("trip insidee trip.jsx: ", trip.driverName);
      return (
-       <div className="tripEntry">
-            <li>
-            Driver name: {trip.driverName}, &nbsp;
-            Trip dates: {trip.tripDate}, &nbsp;
-            Pick-up: {trip.startSt}, &nbsp; {trip.startCity}, &nbsp; {trip.State}, &nbsp;
-            Drop-off: {trip.endSt}, &nbsp; {trip.endCity}, &nbsp; {trip.endState}, &nbsp;
-            Seats available: {trip.numSeats}, &nbsp;
-            Price of seat: {trip.seatPrice}, &nbsp;
-            Vehicle Details: {trip.vehicleMake}, &nbsp; {trip.vehicleModel}, &nbsp; {trip.vehicleYear}, &nbsp;
-            Other trip details: {trip.description};
-            </li>
-       </div>
+       <section className="tripEntry">
+
+            <div className="driverName">
+              <div>Driver name:</div>
+              <p>{trip.driverName}</p>
+            </div>
+
+            <div className="seats">
+              <div>Seats available:</div>
+              <p>{trip.numSeats}<p>
+            </div>
+
+            <div className="price">
+              <div>Price of seat:</div>
+              <p>${trip.seatPrice}</p>
+            </div>
+
+            <div className="vehicle">
+              <div>Vehicle Details:</div>
+              <p>{trip.vehicleMake}, &nbsp; {trip.vehicleModel}, &nbsp; {trip.vehicleYear}</p>
+            </div>
+
+            <div className="tripDates">
+              <div>Trip dates:</div>
+              <p>{trip.tripDate}</p>
+            </div>
+
+            <div className="tripStart">
+              <div>Pick-up:</div>
+              <p>{trip.startSt}, &nbsp; {trip.startCity}, &nbsp; {trip.State}</p>
+            </div>
+
+            <div className="tripEnd">
+              <div>Drop-off:</div>
+              <p> {trip.endSt}, &nbsp; {trip.endCity}, &nbsp; {trip.endState}</p>
+            </div>
+
+            <div className="tripDetails">
+              <div>Other trip details</div>
+              <p>{trip.description}</p>
+            </div>
+
+       </section>
+
      );
   }
 
