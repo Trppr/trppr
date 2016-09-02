3e9e3f608e75cdd1d372532e707cd3188a2e0774
SeanCox <sean.cox@mac.com>
2016-09-02 08:56:25 -0700

    [feature] added stying to create account

--- client/src/components/signUp.jsx
+++ client/src/components/signUp.jsx
@@ -25,30 +25,35 @@ class Signup extends Component {
 
   render() {
     return (
-      <form className="signUp">
+      <div className="container">
+      <form className="signUp form-group">
         <h1>Create Your Account</h1>
         <div>
           <input
             value = {this.state.firstName}
             placeholder = 'Your First Name'
+            className="form-control"
             onChange = {this.handleChange.bind(this, 'firstName')}/>
         </div>
         <div>
           <input
             value = {this.state.lastName}
             placeholder = 'Your Last Name'
+            className="form-control"
             onChange = {this.handleChange.bind(this, 'lastName')}/>
         </div>
         <div>
           <input
             value = {this.state.description}
             placeholder = 'What trips interest you?'
+            className="form-control"
             onChange = {this.handleChange.bind(this, 'description')}/>
         </div>
         <div>
           <input
             value = {this.state.email}
             placeholder = 'Your Email'
+            className="form-control"
             onChange = {this.handleChange.bind(this, 'email')}/>
         </div>
         <div>
@@ -56,6 +61,7 @@ class Signup extends Component {
             value = {this.state.password}
             type = 'password'
             placeholder = 'Create Password'
+            className="form-control"
             onChange = {this.handleChange.bind(this, 'password')}/>
         </div>
         {/* Trying to add a field that will confirm the new user's password */}
@@ -64,6 +70,7 @@ class Signup extends Component {
 
             type = 'password'
             placeholder = 'Confirm password'
+            className="form-control"
             //onChange = {this.handleChange.bind(this, ''''')}
           />
         </div>
@@ -71,9 +78,11 @@ class Signup extends Component {
           <input
             type="button"
             value="Sign Up"
+            className="btn btn-primary"
             onClick = {event => this.submitNewUser()}/>
         </div>
       </form>
+    </div>
     )
   }
 }
