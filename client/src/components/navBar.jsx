aa9425c8b49f6ef1ee00b1d0076ec6a53be4a2b6
Calvin Lee <calvinlee@Calvins-MacBook-Air.local>
2016-09-04 11:39:55 -0700

    [feat]Showing different stuff on navbar based on login status

--- client/src/components/navBar.jsx
+++ client/src/components/navBar.jsx
@@ -4,21 +4,34 @@ import {Link} from 'react-router';
 
 import Login from './login.jsx'
 
-const NavBar = (props) => {
+class NavBar extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
 
-  return (
-    <nav className = 'navbar navbar-default navbar-fixed-top'>
-      <div className = 'container-fluid'>
-        <a className = 'navbar-brand'
-           href = '/'> Trppr </a>
-        <div className = 'navbar-form navbar-right'>
-          <Link to="/createTrip">Create Trip</Link>
-          <Link to='/signUp'>Sign Up</Link>
-          <Login/>
+  checkAuth() {
+    console.log(localStorage.getItem('token'))
+    if(localStorage.getItem('token')) {
+      return <div><Link to='/logOut'>Log Out</Link></div>;
+    } else {
+      return <div><Link to='/signUp'>Sign Up</Link> <Login/></div>;
+    }
+  }
+  render() {
+    return (
+      <nav className = 'navbar navbar-default navbar-fixed-top'>
+        <div className = 'container-fluid'>
+          <a className = 'navbar-brand'
+             href = '/'> Trppr </a>
+          <div className = 'navbar-form navbar-right'>
+            <Link to="/createTrip">Create Trip</Link>
+            {this.checkAuth()}
+          </div>
         </div>
-      </div>
-    </nav>
-  );
+      </nav>
+    );
+  }
 }
 
 export default NavBar;
