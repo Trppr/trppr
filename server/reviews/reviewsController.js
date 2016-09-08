6addfc9f12e98334c8ae0e83661e71990afb1cf6
Lauren Settle <laurjsett@gmail.com>
2016-09-08 14:57:45 -0700

    [pull] add relationship between userID and review

--- server/reviews/reviewsController.js
+++ server/reviews/reviewsController.js
@@ -6,6 +6,11 @@ const sequelize = require('sequelize');
 
 module.exports = {
   createReview: function(req, res) {
+    User.findOne({
+      where : {
+        id: req.body.userId
+      }
+    })
     const newReview = Review.build({
       description: req.body.description
     });
