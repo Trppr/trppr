3192ffeb65499188d3ff72b8046af5ed73f1cdff
Lee Mordell <lee.mordell@gmail.com>
2016-09-07 19:31:51 -0700

    [fix] issue with landing page not receiving input value

--- client/landing.jsx
+++ client/landing.jsx
@@ -15,12 +15,12 @@ class Landing extends Component {
     super(props);
     this.state = { endLocation: '' };
     this.submitData = this.submitData.bind(this);
+    this.handleChange = this.handleChange.bind(this);
   }
 
-  handleChange(name, e) {
-    let change = {};
-    change[name] = e.target.value;
-    this.setState(change);
+  handleChange(value) {
+    console.log('current input val', value);
+    this.setState( {endLocation: value} );
   }
 
   submitData(e) {
@@ -38,13 +38,19 @@ class Landing extends Component {
           <div className="container">
             <h1> Where are you going? </h1>
               <form onSubmit={this.submitData}>
-               <Geosuggest
+
+                <Geosuggest 
+                type="text"
+                name="search"
                 className="form-control"
-                placeholder="Enter a city or state"
-                value = {this.state.endLocation}
-                onChange = {this.handleChange.bind(this, 'endLocation')}
+                placeholder = "Enter a city name"
                 onSuggestSelect={this.onSuggestSelect}
+                value = {this.state.endLocation}
+                onChange = {this.handleChange} 
+                onFocus={this.onFocus}
+                onBlur={this.onBlur}
               />
+
               </form>
           </div>
         </div>
---
+++ client/src/components/tripMap.jsx
@@ -0,0 +1,4 @@
+import React, {Component} from 'react';
+import {render} from 'react-dom';
+import moment from 'moment';
+import {GooglMapLoader, GoogleMap, Marker} from 'react-google-maps';
